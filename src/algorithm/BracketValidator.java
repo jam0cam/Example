c959c040808b3adb97ee7e6827f49d3264b9d6d3
Jia Tse <jam0cam@yahoo.com>
2016-12-02 17:11:51 -0500

    added time/space complexity analysis

--- src/algorithm/BracketValidator.java
+++ src/algorithm/BracketValidator.java
@@ -4,19 +4,22 @@ package algorithm;
  * You're working with an intern that keeps coming to you with JavaScript code that won't run because the braces,
  * brackets, and parentheses are off. To save you both some time, you decide to write a braces/brackets/parentheses
  * validator.
-
- Let's say:
-
- '(', '{', '[' are called "openers."
- ')', '}', ']' are called "closers."
- Write an efficient function that tells us whether or not an input string's openers and closers are properly nested.
-
- Examples:
-
- "{ [ ] ( ) }" should return true
- "{ [ ( ] ) }" should return false
- "{ [ }" should return false
-
+ *
+ * Let's say:
+ *
+ * '(', '{', '[' are called "openers."
+ * ')', '}', ']' are called "closers."
+ * Write an efficient function that tells us whether or not an input string's openers and closers are properly nested.
+ *
+ * Examples:
+ *
+ * "{ [ ] ( ) }" should return true
+ * "{ [ ( ] ) }" should return false
+ * "{ [ }" should return false
+ *
+ * Time: O(n)
+ * Space: O(n)
+ *
  * Created by Jia on 12/2/16.
  */
 public class BracketValidator {
